c0b87b9a9d2f8870732f3bf9fe47b437f91f5b24
StaleJ <stale.jacobsen@student.uib.no>
2021-01-03 00:08:25 +0100

    styleguide applied

--- src/Button.elm
+++ src/Button.elm
@@ -4,9 +4,10 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Event exposing (onClick)
 
--- Main 
-
-main = 
-    Browser.sandbox { init = init, update = update, view = view}
 
 
+-- Main
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
--- src/Pomodo.elm
+++ src/Pomodo.elm
@@ -1,9 +1,12 @@
 module Pomodoro exposing (..)
 
 -- TODO: Making a simple pomodoro timer
+
 import Browser
 import Html exposing (..)
 import Task
 import Time
+
+
 
 -- some code
