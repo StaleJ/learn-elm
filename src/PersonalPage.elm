9f1c244ef31e1d9b3176701ac143277631ad5edc
Ståle <stale.jacobsen@uib.student.no>
2021-01-02 10:59:32 +0100

    added gitignore and some styling

--- src/PersonalPage.elm
+++ src/PersonalPage.elm
@@ -5,12 +5,14 @@ import Html.Attributes exposing (..)
 
 view model = 
     div [ class "jumbotron"]
-        [h1 [] [text "Ståle Jacobsen"]
+        [h1 [style "height" "90px" , style "width" "100%", style "color" "blue", style "display" "flex"] [text "Ståle Jacobsen"]
         , p [] [ text "Hi there, I'm Ståle 👋"]
         , p [] 
             [ text "Student of Computer Science at " 
             , a [ href "https://www.uib.no/en" ] [ text "UNIVERSITY OF BERGEN" ]
             , text "."
             ]]
+ 
+
 
 main = view "website"
